1933dceb18293736092021a5d27a366cc4a01971
NarasimhaRaoNiki <73018939+NarasimhaRaoNiki@users.noreply.github.com>
2020-10-23 07:05:17 +0530

    Update CurrencySwaggerConfig.java

--- ForeignCurrency/src/main/java/com/app/swagger/CurrencySwaggerConfig.java
+++ ForeignCurrency/src/main/java/com/app/swagger/CurrencySwaggerConfig.java
@@ -1,41 +1,48 @@
 package com.app.swagger;
 
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.base.Predicate;
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
 public class CurrencySwaggerConfig { 
 
-	@Bean
-	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
-				.apiInfo(apiInfo()).select().paths(postPaths()).build();
-	}
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.app.controller"))
+                .paths(regex("/.*"))
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Foreign Currency REST API")
+                .description("\"Display Foreign Currency REST API for Current/LastSixMonths Information\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Narasimha Punyamurthy", "http://localhost:8080/currency", "npunyamurthy9@gmail.com"))
+                .build();
+    }
+   
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
 
-	private Predicate<String> postPaths() {
-		return or(regex("/"), regex("/api/*"));
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("****")
-				.description("****")
-				.termsOfServiceUrl("****")
-				.contact("****").license("**** License")
-				.licenseUrl("****").version("1.0").build();
-	}
-
+	
+	 }
 }
